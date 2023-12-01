f05c91ab67d8b1c5411b175bbef115d382df7106
kevinavignon <kavignon@petalmd.com>
2023-12-01 12:18:52 -0500

    Refactored impl. to be more functional.

--- AdventOfCode/code/Ruby/day_1/implementation.rb
+++ AdventOfCode/code/Ruby/day_1/implementation.rb
@@ -1,20 +1,16 @@
-# frozen_string_literal: true
 module CalibrationDocument
   def self.get_calibration_value(file_path)
-    if File.exist?(file_path)
-      calibration_value = 0
-      IO.foreach(file_path) do |line|
-        calibration_value += compute_calibration(line)
-      end
-      return calibration_value
-    else
-      puts "File not found: #{file_path}"
-    end
+    return puts("File not found: #{file_path}") unless File.exist?(file_path)
+
+    IO
+      .foreach(file_path)
+      .map { |line| compute_calibration(line) }
+      .sum
   end
 
   private
 
-  def self.compute_calibration(calibration_line = '')
+  def self.compute_calibration(calibration_line)
     return 0 if calibration_line.nil? || calibration_line.empty?
 
     first, second = scan_line_for_values(calibration_line)
@@ -23,9 +19,8 @@ module CalibrationDocument
 
   def self.scan_line_for_values(calibration_line)
     digits_in_line = calibration_line.chars.select { |c| c.match?(/\d/) }
-
     [digits_in_line.first.to_i * 10, digits_in_line.last.to_i]
   end
 end
 
-puts CalibrationDocument.get_calibration_value('input/2023/day_1_part_1.txt')
+CalibrationDocument.get_calibration_value('input/2023/day_1_part_1.txt')
