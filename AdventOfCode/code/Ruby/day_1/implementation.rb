72f1669a6f2bba1c83ffcb5ca947f8f7a89efa86
kevinavignon <kavignon@petalmd.com>
2023-12-01 17:48:37 -0500

    Trying to wrap my head against the regex.

--- AdventOfCode/code/Ruby/day_1/implementation.rb
+++ AdventOfCode/code/Ruby/day_1/implementation.rb
@@ -1,4 +1,19 @@
 module CalibrationDocument
+  NUMBER_WORDS = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }
+
+  NUMBER_AS_DIGIT_OR_TEXT_REGEX = /\b(?:one|two|three|four|five|six|seven|eight|nine|\d+)\b|\d+|[a-zA-Z]+/
+
+
   def self.get_file_calibration_sum(file_path)
     return puts("File not found: #{file_path}") unless File.exist?(file_path)
 
@@ -13,23 +28,33 @@ module CalibrationDocument
   def self.compute_calibration(calibration_line)
     return 0 if calibration_line.nil? || calibration_line.empty?
 
-    first, second = scan_line_for_values(calibration_line)
-    first + second
+    readings = collect_readings(calibration_line)
+    first, last = readings.first, readings.last
+
+    val =
+      if first && last
+        (first.to_i * 10) + last.to_i
+      elsif last.nil?
+        (first.to_i * 10) + first.to_i
+      else
+        0
+      end
+    val
   end
 
-  def self.scan_line_for_values(calibration_line)
-    calibration_readings = calibration_line.chars.select { |c| c.match?(/\d/) }
-
-    [first_calibration_reading(calibration_readings), last_calibration_reading(calibration_readings)]
+  def self.collect_readings(line)
+    line.scan(/\d+|[a-zA-Z]+/).chunk { |value| value.match?(/\d+/) }.flat_map do |is_digit, group|
+      if is_digit
+        group.join.to_i
+      else
+        group.map { |text| convert_spelled_out_number(text) }
+      end
+    end.compact
   end
 
-    def self.first_calibration_reading(calibration_vals)
-      calibration_vals.first.to_i * 10
-    end
-
-    def self.last_calibration_reading(calibration_vals)
-      calibration_vals.last.to_i
-    end
+  def self.convert_spelled_out_number(spelled_out_number)
+    NUMBER_WORDS[spelled_out_number.downcase]
+  end
 end
 
 puts CalibrationDocument.get_file_calibration_sum('input/2023/day_1.txt')
