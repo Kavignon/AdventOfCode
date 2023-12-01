7984a9c95c99a023a1812a14e97f398e55755d4c
kevinavignon <kavignon@petalmd.com>
2023-12-01 15:33:58 -0500

    WIP - putting together an impl for part 2.

--- AdventOfCode/code/Ruby/day_1/implementation.rb
+++ AdventOfCode/code/Ruby/day_1/implementation.rb
@@ -1,5 +1,20 @@
 module CalibrationDocument
-  def self.get_calibration_value(file_path)
+  TEXT_TO_NUMBER_HASH = {
+    :zero => 0,
+    :one => 1,
+    :two => 2,
+    :three => 3,
+    :four => 4,
+    :five => 5,
+    :six => 6,
+    :seven => 7,
+    :eight => 8,
+    :nine => 9
+  }
+
+  TEXT_TO_NUMBER_REGEX = /\b(?:#{TEXT_TO_NUMBER_HASH.keys.map { |word| Regexp.escape(word.to_s) }.join('|')})\b|\d+/
+
+  def self.get_file_calibration_sum(file_path)
     return puts("File not found: #{file_path}") unless File.exist?(file_path)
 
     IO
@@ -18,9 +33,21 @@ module CalibrationDocument
   end
 
   def self.scan_line_for_values(calibration_line)
-    digits_in_line = calibration_line.chars.select { |c| c.match?(/\d/) }
-    [digits_in_line.first.to_i * 10, digits_in_line.last.to_i]
+    calibration_vals = calibration_line.scan(TEXT_TO_NUMBER_REGEX)
+
+    first_digit_index = first_calibration_val(calibration_vals)
+    last_digit_index = last_calibration_val(calibration_vals)
+
+    [first_digit_index, last_digit_index]
+  end
+
+  def self.first_calibration_val(calibration_vals)
+
+  end
+
+  def self.last_calibration_val(calibration_vals)
+
   end
 end
 
-CalibrationDocument.get_calibration_value('input/2023/day_1.txt')
+puts CalibrationDocument.get_file_calibration_sum('input/2023/day_1.txt')
