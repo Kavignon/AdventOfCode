d459361724508e881e0eaa5b5a240cd8ded50b6d
kevinavignon <kavignon@petalmd.com>
2023-12-01 18:26:50 -0500

    Moved away from the regex and trying a sliding window approach.
    While debugging, I'm realizing that there's some missing content and somehow, we collect a digit with a missing index (nil). Will fix later.

--- AdventOfCode/code/Ruby/day_1/implementation.rb
+++ AdventOfCode/code/Ruby/day_1/implementation.rb
@@ -11,15 +11,12 @@ module CalibrationDocument
     'nine' => 9
   }
 
-  NUMBER_AS_DIGIT_OR_TEXT_REGEX = /\b(?:one|two|three|four|five|six|seven|eight|nine|\d+)\b|\d+|[a-zA-Z]+/
-
-
   def self.get_file_calibration_sum(file_path)
     return puts("File not found: #{file_path}") unless File.exist?(file_path)
 
     IO
       .foreach(file_path)
-      .map { |line| compute_calibration(line) }
+      .map { |line| collect_reading(line) }
       .sum
   end
 
@@ -28,7 +25,7 @@ module CalibrationDocument
   def self.compute_calibration(calibration_line)
     return 0 if calibration_line.nil? || calibration_line.empty?
 
-    readings = collect_readings(calibration_line)
+    readings = extract_digits_from_line(calibration_line)
     first, last = readings.first, readings.last
 
     val =
@@ -42,14 +39,60 @@ module CalibrationDocument
     val
   end
 
-  def self.collect_readings(line)
-    line.scan(/\d+|[a-zA-Z]+/).chunk { |value| value.match?(/\d+/) }.flat_map do |is_digit, group|
-      if is_digit
-        group.join.to_i
+  def self.collect_reading(line)
+    digits_found_in_line = extract_digits_and_indexes(line)
+    numbers_found_in_line = extract_digits_from_line(line)
+
+    first_digit = digits_found_in_line.find { |digit1| numbers_found_in_line.any? { |digit2| digit1[:index] < digit2[:index] } }
+    last_digit = digits_found_in_line.reverse.find { |digit1| numbers_found_in_line.any? { |digit2| digit1[:index] > digit2[:index] } }
+
+    val =
+      if first_digit && last_digit
+        (first_digit[:digit].to_i * 10) + last_digit[:digit].to_i
+      elsif last_digit.nil?
+        (first_digit[:digit].to_i * 10) + first_digit[:digit].to_i
       else
-        group.map { |text| convert_spelled_out_number(text) }
+        0
       end
-    end.compact
+
+    val
+  end
+
+  def self.extract_digits_and_indexes(line)
+    readings = []
+
+    i = 0
+    while i < line.length
+      char = line[i]
+
+      if ('0'..'9').cover?(char)
+        readings.push({ index: i, digit: char.to_i })
+      end
+
+      i += 1
+    end
+
+    readings
+  end
+
+  def self.extract_digits_from_line(line)
+    readings = []
+
+    i = 0
+    while i < line.length
+      window_size = 5
+      window = line[i, window_size]
+
+      digit = convert_spelled_out_number(window)
+      if digit
+        index_of_letter_o = line.index('o', i) # Example, replace 'o' with the actual letter to find
+        readings.push({ index: index_of_letter_o, digit: digit })
+      end
+
+      i += 1
+    end
+
+    readings
   end
 
   def self.convert_spelled_out_number(spelled_out_number)
