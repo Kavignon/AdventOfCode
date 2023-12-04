c9495f6736ce5b094f498a0f1b330e2d0e346087
kevinavignon <kavignon@petalmd.com>
2023-12-04 14:18:06 -0500

    feat: Refactor Day 1 solution for Advent of Code 2023
    - Simplified the calibration document processing logic.
    - Combined initialization and assignment for array construction.
    - Utilized a `case` statement for improved readability in value combination.
    - Avoided redundant array operations by using `compact` during array construction.
    - Addressed edge cases for efficient handling of empty and single-value readings.
    
    These changes aim to enhance code readability and maintainability while ensuring efficient processing of calibration documents.

--- src/Ruby/2023/day_1/implementation.rb
+++ src/Ruby/2023/day_1/implementation.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 # typed: true
 
+require "sorbet-runtime"
+
 # Represents a measurement in a calibration document.
 class CalibrationReading
-  extend T::Sig
+  extend ::T::Sig
 
   sig { params(index: Integer, digit: Integer).void }
   def initialize(index, digit)
@@ -25,112 +27,51 @@ module CalibrationDocument
 
   NUMBER_WORDS = T.let(
     {
-      'one' => 1,
-      'two' => 2,
-      'three' => 3,
-      'four' => 4,
-      'five' => 5,
-      'six' => 6,
-      'seven' => 7,
-      'eight' => 8,
-      'nine' => 9
+      'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
+      'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9
     }.freeze,
     T::Hash[String, Integer]
   )
 
-  DIGITS_RANGE = T.let((0..9).freeze, T::Range[Integer])
+  MAX_WORD_LENGTH_0_TO_9 = T.let(5, Integer)
 
   sig { params(file_path: String).returns(Integer) }
-  def self.get_file_calibration_sum(file_path)
-    unless File.exist?(file_path)
-      puts("File not found: #{file_path}")
-      0
-    end
+  def self.launcher_calibration_value(file_path)
+    return 0 unless File.exist?(file_path)
 
-    IO
-      .foreach(file_path)
-      .map { |line| collect_reading(line) }
-      .sum
-  end
-
-  sig { params(calibration_line: String).returns(Integer) }
-  def self.compute_calibration(calibration_line)
-    return 0 if calibration_line.empty?
-
-    readings = extract_digits_from_line(calibration_line)
-
-    return 0 if readings.empty?
-    return (readings.last.to_i * 10) + readings.last.to_i if readings.length == 1
-
-    (readings.first.to_i * 10) + readings.last.to_i
+    IO.foreach(file_path).map { |line| calibration_value(line) }.sum
   end
 
   sig { params(line: String).returns(Integer) }
-  def self.collect_reading(line)
-    digits_found_in_line = extract_digits_and_indexes(line)
-    numbers_found_in_line = extract_digits_from_line(line)
+  def self.calibration_value(line)
+    readings = extract_readings(line)
 
-    first_digit = digits_found_in_line.find do |digit1|
-      numbers_found_in_line.any? do |digit2|
-        digit1[:index] < digit2[:index]
-      end
-    end
-    last_digit = digits_found_in_line.reverse.find do |digit1|
-      numbers_found_in_line.any? do |digit2|
-        digit1[:index] > digit2[:index]
-      end
-    end
-
-    if first_digit && last_digit
-      (first_digit[:digit].to_i * 10) + last_digit[:digit].to_i
-    elsif last_digit.nil?
-      (T.must(first_digit)[:digit].to_i * 10) + T.must(first_digit)[:digit].to_i
-    else
-      0
+    case readings.size
+    when 0 then 0
+    when 1 then readings.first.digit * 11
+    else (readings.first.digit.to_s + readings.last.digit.to_s).to_i
     end
   end
 
-  sig { params(line: String).returns(T::Array[CalibrationReading[T.untyped]]) }
-  def self.extract_digits_and_indexes(line)
+  sig { params(line: String).returns(T::Array[CalibrationReading]) }
+  def self.extract_readings(line)
     readings = []
 
-    i = 0
-    while i < line.length
-      char = line[i]
-
-      readings.push({ index: i, digit: char.to_i }) if ('0'..'9').cover?(char)
-
-      i += 1
+    line.each_char.with_index do |char, i|
+      if char.match?(/\d/)
+        readings << CalibrationReading.new(i, char.to_i)
+      else
+        (1..MAX_WORD_LENGTH_0_TO_9).each do |len|
+          window = line[i, len]
+          spelled_out_number = window.downcase
+          digit = NUMBER_WORDS[spelled_out_number]
+          readings << CalibrationReading.new(i, digit) if digit
+        end
+      end
     end
 
     readings
-  end
-
-  sig { params(line: String).returns(T::Array[CalibrationReading[T.untyped]]) }
-  def self.extract_digits_from_line(line)
-    readings = []
-
-    i = 0
-    while i < line.length
-      window_size = 5
-      window = line[i, window_size]
-
-      digit = convert_spelled_out_number(window)
-      if digit
-        index_of_letter_o = line.index('o', i) # Example, replace 'o' with the actual letter to find
-        readings.push({ index: index_of_letter_o, digit: digit })
-      end
-
-      i += 1
-    end
-
-    readings
-  end
-
-  sig { params(spelled_out_number: String).returns(T.nilable(Integer)) }
-  def self.convert_spelled_out_number(spelled_out_number)
-    NUMBER_WORDS[spelled_out_number.downcase]
   end
 end
 
-puts CalibrationDocument.get_file_calibration_sum('input/2023/day_1.txt')
+puts CalibrationDocument.launcher_calibration_value('src/input/2023/day_1.txt')
