723ea25e3f3e4e79f40f0401eb4a01284aa71e03
kevinavignon <kavignon@petalmd.com>
2023-12-05 07:35:18 -0500

    Completed part II of day 2.
    - Added functionality to evaluate the minimum set of cubes required for each game on Snow Island.
    - Updated the IceBagEvaluation module to calculate the power of each minimum cube set.
    - Implemented the record_power_set and find_min_color_for_cubes methods to handle game records.
    - Utilized constants and type annotations for improved code clarity.
    - Successfully computed the sum of the power of minimum cube sets for the given games.

--- src/Ruby/2023/day_2/implementation.rb
+++ src/Ruby/2023/day_2/implementation.rb
@@ -9,62 +9,49 @@ module IceBagEvaluation
 
   GAME_ID_REGEX = T.let(/Game (\d+):(.*)/, Regexp)
 
-  MAX_RED_BALLS = T.let(12, Integer)
-  MAX_GREEN_BALLS = T.let(13, Integer)
-  MAX_BLUE_BALLS = T.let(14, Integer)
+  INITIAL_COLOR_TO_MIN_COUNT = T.let({ blue: 0, red: 0, green: 0 }, T::Hash[Symbol, Integer])
 
-  def self.ice_ball_game_sum(file_path)
+  def self.ice_cube_acc_power(file_path)
     return 0 unless File.exist?(file_path)
 
     IO
       .foreach(file_path)
-      .map { |game_record| ice_cube_game_id(game_record) }
+      .map { |game_record| record_power_set(game_record) }
       .sum
   end
 
   sig { params(game_record: String).returns(Integer) }
   # Defines a method to parse the content of a game record and extract the game id when the game can be played.
-  def self.ice_cube_game_id(game_record)
+  def self.record_power_set(game_record)
     match = game_record.match(GAME_ID_REGEX)
     return unless match
     return unless match.length >= 2
 
-    game_id = match[1].to_i
     game_sets = match[2].split(';').map(&:strip)
+    color_to_min_count = INITIAL_COLOR_TO_MIN_COUNT.dup
 
     game_sets.each do |game_set|
-      next if ice_cube_set_valid?(game_set)
-
-      return 0
+      color_to_min_count = find_min_color_for_cubes(game_set, color_to_min_count)
     end
 
-    game_id
+    color_to_min_count[:red] * color_to_min_count[:green] * color_to_min_count[:blue]
   end
 
   # Defines a method to evaluate whether a give game set of ice cube can be played on Snow Island.
   # @param [T::Array[String]] game_set
   # @return [T::Boolean]
-  def self.ice_cube_set_valid?(game_set)
-    ice_cube_set_hash = { blue: 0, red: 0, green: 0 }
+  def self.find_min_color_for_cubes(game_set, color_to_min_count)
+    cubes = game_set.split(',').map(&:strip)
 
-    game_set.split(',').map(&:strip).each do |ice_cube_set|
-      count, color = ice_cube_set.split(' ')
-      ice_cube_set_hash[color.to_sym] += count.to_i
+    cubes.each do |cube|
+      count, color = cube.split(' ')
+      count = count.to_i
 
-      case
-      when ice_cube_set_hash[:green] > MAX_GREEN_BALLS
-        return false
-      when ice_cube_set_hash[:blue] > MAX_BLUE_BALLS
-        return false
-      when ice_cube_set_hash[:red] > MAX_RED_BALLS
-        return false
-      else
-        next
-      end
+      color_to_min_count[color.to_sym] = [color_to_min_count[color.to_sym], count].max
     end
 
-    true
+    color_to_min_count
   end
 end
 
-puts IceBagEvaluation.ice_ball_game_sum('src/input/2023/day_2.txt')
+puts IceBagEvaluation.ice_cube_acc_power('src/input/2023/day_2.txt')
